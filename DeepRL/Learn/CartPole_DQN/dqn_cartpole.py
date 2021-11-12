d29d9b59fbe1785a2c632c9817ae8011bf6491d2
nolme <80020062+wisemountain@users.noreply.github.com>
2021-11-12 17:23:57 +0900

    dqn_cartpole 추가.

---
+++ DeepRL/Learn/CartPole_DQN/dqn_cartpole.py
@@ -0,0 +1,11 @@
+import sys
+import gym
+import pylab
+import random
+import numpy as np
+from collections import deque 
+from tensorflow.keras import Dense 
+from tensorflow.keras.optimizers import Adam 
+from tensorflow.keras.models import Sequential 
+
+EPISODES = 300
